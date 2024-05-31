8268f3ec3896cbfc530c1c21182539d20e1e8491
Sahand Namvar <sahandnamvar97@gmail.com>
2024-05-30 21:53:05 -0400

    Created Navbar. Setup DarkMode toggle.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+
 export const App = () => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div
+      className={`${theme === "dark" ? "" : "dark"}
+      overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900`}
+    >
+      <div className="fixed top-0 -z-10 h-full w-full">
+        {theme === "dark" ? (
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        ) : (
+          <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+        )}
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar theme={theme} toggleTheme={toggleTheme} />
+      </div>
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdOutlineDarkMode, MdDarkMode } from "react-icons/md";
+
+const Navbar = ({ theme, toggleTheme }) => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a
+          href="#"
+          className="text-4xl font-medium tracking-wide dark:text-pink-900"
+        >
+          Sn
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl dark:text-pink-900">
+        <FaLinkedin />
+        <FaGithub />
+        <span>
+          {theme === "dark" ? (
+            <MdOutlineDarkMode onClick={toggleTheme} />
+          ) : (
+            <MdDarkMode onClick={toggleTheme} className="dark:text-pink-900" />
+          )}
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
